8839743ed7a3364b3412f9da64d27e6bd5a42c07
abmysong <abmysong@gmail.com>
2022-11-09 20:57:02 +0900

    [#2] boards read

--- js/board.js
+++ js/board.js
@@ -4,3 +4,25 @@ const selectAll = function(selectAll) {
     checkbox.checked = selectAll.checked;
   });
 };
+
+const boardsRead = function() {
+  axios.get('http://localhost:3000/api/boards').then(function(response) {
+    const tbodyParent = document.getElementById('tbody-parent');
+    tbodyParent.innerHTML = '';
+    const trChild = document.getElementById('tr-child');
+    for (let index in response.data.boards) {
+      console.log(index);
+      const board = response.data.boards[index];
+      const newTrChild = trChild.cloneNode(true);
+      tbodyParent.appendChild(newTrChild);
+      document.getElementsByName('a-index')[index].innerHTML = board.board_pk;
+      document.getElementsByName('a-index')[index].href = './detail.html?board_pk=' + board.board_pk;
+      document.getElementsByName('a-title')[index].innerHTML = board.title;
+      document.getElementsByName('a-title')[index].href = './detail.html?board_pk=' + board.board_pk;
+      document.getElementsByName('td-id')[index].innerHTML = board.id;
+      document.getElementsByName('td-create-at')[index].innerHTML = moment(board.create_at).format('YYYY-MM-DD HH:mm');
+    }
+  }).catch(function(error) {
+    alert(error.response.data.message);
+  });
+};
